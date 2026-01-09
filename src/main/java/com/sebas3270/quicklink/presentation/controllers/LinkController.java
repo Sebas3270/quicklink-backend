02822554d2695bacfe706a10cd5ee78530f2119a
Sebastian <salvarezocampo@hotmail.com>
2026-01-08 19:36:18 -0600

    adding new response to link controller

--- src/main/java/com/sebas3270/quicklink/presentation/controllers/LinkController.java
+++ src/main/java/com/sebas3270/quicklink/presentation/controllers/LinkController.java
@@ -1,7 +1,10 @@
 package com.sebas3270.quicklink.presentation.controllers;
 
+import com.sebas3270.quicklink.domain.entities.Link;
 import com.sebas3270.quicklink.presentation.dtos.CreateLinkDto;
 import com.sebas3270.quicklink.domain.repositories.LinksRepository;
+import com.sebas3270.quicklink.presentation.dtos.LinkResponseDto;
+import com.sebas3270.quicklink.presentation.dtos.mappers.LinkDtoMappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/shorten")
 @Tag(name = "Links", description = "Links operations")
 public class LinkController {
 
@@ -23,17 +27,15 @@ public class LinkController {
 
     @GetMapping("/{shortLink}")
     @Operation(description = "Gets the original link when an exist short url provided")
-    ResponseEntity<String> getLinkUrl(@PathVariable String shortLink){
-        return ResponseEntity.ok(linksRepository.getLink(shortLink).getOriginalUrl());
+    ResponseEntity<LinkResponseDto> getLinkUrl(@PathVariable String shortLink){
+        Link link = linksRepository.getLink(shortLink);
+        return ResponseEntity.ok(LinkDtoMappers.entityToMapper(link));
     }
 
-    @PostMapping("/api/links")
+    @PostMapping("/")
     @Operation(description = "Saves the link provided a returns the complete short url")
-    ResponseEntity<String> saveLink(@Valid @RequestBody CreateLinkDto createLinkDto){
-        String shortLink = linksRepository
-                .postLink(createLinkDto.url()).getShortUrl();
-        String fullShortUrl = env.getProperty("app.base-url")
-                + "/" + shortLink;
-        return ResponseEntity.ok(fullShortUrl);
+    ResponseEntity<LinkResponseDto> saveLink(@Valid @RequestBody CreateLinkDto createLinkDto){
+        Link link = linksRepository.postLink(createLinkDto.url());
+        return ResponseEntity.ok(LinkDtoMappers.entityToMapper(link));
     }
 }
